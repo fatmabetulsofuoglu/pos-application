1f0db6962d53f8fb7000f432e0596bb885b931b4
Fatma Betül Sofuoğlu <40726607+fatmabetulsofuoglu@users.noreply.github.com>
2024-03-06 23:09:32 +0300

    Cart Total

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import { CartTotal } from "./components/cart/CartTotal";
 import { Categories } from "./components/categories/Categories";
 import Header from "./components/header/Header";
 import { Products } from "./components/products/Products";
@@ -7,14 +8,14 @@ function App() {
     <>
       <Header />
       <div className="home px-6 flex justify-between gap-10">
-        <div className="categories flex-[2] overflow-auto max-h-[calc(100vh-_-112px)] pb-20">
+        <div className="categories overflow-auto max-h-[calc(100vh-_-112px)] pb-20">
           <Categories></Categories>
         </div>
         <div className="products flex-[8]">
           <Products/>
         </div>
-        <div className="cart-totals">
-          <div>cart totals</div>
+        <div className="cart-wrapper min-w-[180px] md:-mr-[24px] md:-mt-[24px] border">
+          <CartTotal/>
         </div>
       </div>
     </>
--- client/src/components/categories/Categories.jsx
+++ client/src/components/categories/Categories.jsx
@@ -3,18 +3,18 @@ import React from 'react'
 export const Categories = () => {
     return (
         <ul className='flex gap-4 flex-col'>
-            <li className='bg-green-700 px-6 py-10 text-white w-full cursor-pointer hover:bg-pink-600 transition-all text-center'><span className="">Tümü</span></li>
-            <li className='bg-green-700 px-6 py-10 text-white w-full cursor-pointer hover:bg-pink-600 transition-all text-center'><span className="">Yiyecek</span></li>
-            <li className='bg-green-700 px-6 py-10 text-white w-full cursor-pointer hover:bg-pink-600 transition-all text-center'><span className="">İçecek</span></li>
-            <li className='bg-green-700 px-6 py-10 text-white w-full cursor-pointer hover:bg-pink-600 transition-all text-center'><span className="">Tümü</span></li>
-            <li className='bg-green-700 px-6 py-10 text-white w-full cursor-pointer hover:bg-pink-600 transition-all text-center'><span className="">Tümü</span></li>
-            <li className='bg-green-700 px-6 py-10 text-white w-full cursor-pointer hover:bg-pink-600 transition-all text-center'><span className="">Yiyecek</span></li>
-            <li className='bg-green-700 px-6 py-10 text-white w-full cursor-pointer hover:bg-pink-600 transition-all text-center'><span className="">İçecek</span></li>
-            <li className='bg-green-700 px-6 py-10 text-white w-full cursor-pointer hover:bg-pink-600 transition-all text-center'><span className="">Tümü</span></li>
-            <li className='bg-green-700 px-6 py-10 text-white w-full cursor-pointer hover:bg-pink-600 transition-all text-center'><span className="">Tümü</span></li>
-            <li className='bg-green-700 px-6 py-10 text-white w-full cursor-pointer hover:bg-pink-600 transition-all text-center'><span className="">Yiyecek</span></li>
-            <li className='bg-green-700 px-6 py-10 text-white w-full cursor-pointer hover:bg-pink-600 transition-all text-center'><span className="">İçecek</span></li>
-            <li className='bg-green-700 px-6 py-10 text-white w-full cursor-pointer hover:bg-pink-600 transition-all text-center'><span className="">Tümü</span></li>
+            <li className='bg-green-700 px-6 py-10 text-white w-full cursor-pointer hover:bg-pink-600 transition-all text-center min-w-[145px]'><span className="">Tümü</span></li>
+            <li className='bg-green-700 px-6 py-10 text-white w-full cursor-pointer hover:bg-pink-600 transition-all text-center min-w-[145px]'><span className="">Yiyecek</span></li>
+            <li className='bg-green-700 px-6 py-10 text-white w-full cursor-pointer hover:bg-pink-600 transition-all text-center min-w-[145px]'><span className="">İçecek</span></li>
+            <li className='bg-green-700 px-6 py-10 text-white w-full cursor-pointer hover:bg-pink-600 transition-all text-center min-w-[145px]'><span className="">Tümü</span></li>
+            <li className='bg-green-700 px-6 py-10 text-white w-full cursor-pointer hover:bg-pink-600 transition-all text-center min-w-[145px]'><span className="">Tümü</span></li>
+            <li className='bg-green-700 px-6 py-10 text-white w-full cursor-pointer hover:bg-pink-600 transition-all text-center min-w-[145px]'><span className="">Yiyecek</span></li>
+            <li className='bg-green-700 px-6 py-10 text-white w-full cursor-pointer hover:bg-pink-600 transition-all text-center min-w-[145px]'><span className="">İçecek</span></li>
+            <li className='bg-green-700 px-6 py-10 text-white w-full cursor-pointer hover:bg-pink-600 transition-all text-center min-w-[145px]'><span className="">Tümü</span></li>
+            <li className='bg-green-700 px-6 py-10 text-white w-full cursor-pointer hover:bg-pink-600 transition-all text-center min-w-[145px]'><span className="">Tümü</span></li>
+            <li className='bg-green-700 px-6 py-10 text-white w-full cursor-pointer hover:bg-pink-600 transition-all text-center min-w-[145px]'><span className="">Yiyecek</span></li>
+            <li className='bg-green-700 px-6 py-10 text-white w-full cursor-pointer hover:bg-pink-600 transition-all text-center min-w-[145px]'><span className="">İçecek</span></li>
+            <li className='bg-green-700 px-6 py-10 text-white w-full cursor-pointer hover:bg-pink-600 transition-all text-center min-w-[145px]'><span className="">Tümü</span></li>
 
         </ul>
     )
