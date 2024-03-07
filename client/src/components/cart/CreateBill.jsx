59afe9b88745c4faefce5a0f80393d2d5925c28e
Fatma Betül Sofuoğlu <40726607+fatmabetulsofuoglu@users.noreply.github.com>
2024-03-07 14:32:22 +0300

    CreateBill.jsx updated and edited modal
    Added form in modal and Form settings, inputs

--- client/src/components/cart/CreateBill.jsx
+++ client/src/components/cart/CreateBill.jsx
@@ -1,4 +1,4 @@
-import {Modal } from "antd";
+import { Form, Modal, Input, Select, Button, Card } from "antd";
 
 export const CreateBill = ({ isModalOpen, setIsModalOpen }) => {
   return (
@@ -7,6 +7,47 @@ export const CreateBill = ({ isModalOpen, setIsModalOpen }) => {
       open={isModalOpen}
       footer={false}
       onCancel={() => setIsModalOpen(false)}
-    ></Modal>
+    >
+      <Form layout="vertical">
+        <Form.Item label="Müşterinin Adı" rules={[{ required: true }]}>
+          <Input placeholder="Müşterinin Adını Yazınız" />
+        </Form.Item>
+        <Form.Item label="Telefon Numarası" rules={[{ required: true }]}>
+          <Input placeholder="Bir Telefon Numarası Yazınız" />
+        </Form.Item>
+        <Form.Item label="Ödeme Yöntemi" rules={[{ required: true }]}>
+          <Select placeholder="Ödeme Yöntemi Seçiniz">
+            <Select.Option value="Nakit">Nakit</Select.Option>
+            <Select.Option value="Kredi Kartı">Kredi Kartı</Select.Option>
+          </Select>
+        </Form.Item>
+        <Card className="w-full">
+            <div className="flex justify-between my-2">
+              <span>Ara Toplam</span>
+              <span> 549.00₺</span>
+            </div>
+            <div className="flex justify-between my-2">
+              <span>KDV %8</span>
+              <span className="text-red-600">+43.92₺</span>
+            </div>
+            <div className="flex justify-between">
+              <span>
+                <b>Toplam</b>
+              </span>
+              <b>
+                <span>+43.92₺</span>
+              </b>
+            </div>
+            <Button
+              type="primary"
+              onClick={() => setIsModalOpen(true)}
+              size="middle"
+              className="bg-green-600 mt-3 w-full"
+            >
+              Fatura Oluştur
+            </Button>
+          </Card>
+      </Form>
+    </Modal>
   );
 };
