52cecf318a724bfc9c739faaacf7ed708220b4b8
Fatma Betül Sofuoğlu <40726607+fatmabetulsofuoglu@users.noreply.github.com>
2024-03-07 19:22:31 +0300

    StatisticCard finished

--- client/src/pages/StatisticPage.jsx
+++ client/src/pages/StatisticPage.jsx
@@ -11,11 +11,27 @@ export const StatisticPage = () => {
           <h3 className="text-xl">
             Hoşgeldin <span className="text-green-700 text-xl">admin</span>
           </h3>
-          <div className="statictic-cards grid grid-cols-4 my-10 gap-10">
-            <StatisticCard title={"Toplam Müşteri"} amount={"10"}/>
-            <StatisticCard title={"Toplam Müşteri"} amount={"10"}/>
-            <StatisticCard title={"Toplam Müşteri"} amount={"10"}/>
-            <StatisticCard title={"Toplam Müşteri"} amount={"10"}/>
+          <div className="statictic-cards grid xl:grid-cols-4 md:grid-cols-2 my-10 md:gap-10 gap-4">
+            <StatisticCard
+              title={"Toplam Müşteri"}
+              amount={"6"}
+              img={"images/user.png"}
+            />
+            <StatisticCard
+              title={"Toplam Kazanç"}
+              amount={"100.000₺"}
+              img={"images/money.png"}
+            />
+            <StatisticCard
+              title={"Toplam Satış"}
+              amount={"156"}
+              img={"images/sale.png"}
+            />
+            <StatisticCard
+              title={"Toplam Ürün"}
+              amount={"67"}
+              img={"images/product.png"}
+            />
           </div>
         </div>
       </div>
