e2b0e5106d36e862dab600c701f671d313c69fb1
Fatma Betül Sofuoğlu <40726607+fatmabetulsofuoglu@users.noreply.github.com>
2024-03-07 13:40:16 +0300

    Created CartPage and HomePage

--- client/src/pages/CartPage.jsx
+++ client/src/pages/CartPage.jsx
@@ -1,5 +1,68 @@
+import Header from "../components/header/Header";
+import { Table, Card, Button } from "antd";
+
 export const CartPage = () => {
+  const dataSource = [
+    {
+      key: "1",
+      name: "Elma",
+      price: 3,
+      mount: 5,
+    },
+    {
+      key: "2",
+      name: "Karpuz",
+      price: 1,
+      mount: 50,
+    },
+  ];
+  const columns = [
+    {
+      title: "Ürün Adı",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Fiyat",
+      dataIndex: "mount",
+      key: "mount",
+    },
+    {
+      title: "Adet",
+      dataIndex: "price",
+      key: "price",
+    },
+    
+  ];
   return (
-    <div>CartPage</div>
-  )
-}
+    <div>
+      <Header />
+      <div className="px-6">
+        <Table dataSource={dataSource} columns={columns} bordered />
+        <div className="cart-total flex justify-end">
+          <Card className="w-72">
+            <div className="flex justify-between my-2">
+              <span>Ara Toplam</span>
+              <span> 549.00₺</span>
+            </div>
+            <div className="flex justify-between my-2">
+              <span>KDV %8</span>
+              <span className="text-red-600">+43.92₺</span>
+            </div>
+            <div className="flex justify-between">
+              <span>
+                <b>Toplam</b>
+              </span>
+              <b>
+                <span>+43.92₺</span>
+              </b>
+            </div>
+            <Button type="primary" size="middle" className="bg-green-600 mt-3 w-full">
+              Sipariş Oluştur
+            </Button>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+};
