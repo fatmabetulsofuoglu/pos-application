020e22f0c36ca78b994bcc09d2ae5a27e1c773eb
Fatma Betül Sofuoğlu <40726607+fatmabetulsofuoglu@users.noreply.github.com>
2024-03-07 18:24:50 +0300

    Created StatisticPage

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -62,13 +62,13 @@ const Header = () => {
             <UserOutlined className="md:text-2xl text-xl" />
             <span className="md:text-xs text-[10px]">Müşteriler</span>
           </Link>
-          <a
-            href={"/"}
+          <Link
+            to={"/statistic"}
             className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
           >
             <BarChartOutlined className="md:text-2xl text-xl" />
             <span className="md:text-xs text-[10px]">İstatistikler</span>
-          </a>
+          </Link>
           <Link
             to={"/"}
             className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
---
+++ client/src/pages/StatisticPage.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/header/Header";
+
+export const StatisticPage = () => {
+  
+  return (
+    <>
+      <Header />
+      <div className="p-6">
+        <h2 className="text-4xl font bold text-center">İstatistikler</h2>
+      </div>
+    
+    
+    </>
+  );
+};
