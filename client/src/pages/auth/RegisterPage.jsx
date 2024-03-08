57c03d9131aa1dd8a836af95c581e64f80c47705
Fatma Betül Sofuoğlu <40726607+fatmabetulsofuoglu@users.noreply.github.com>
2024-03-08 03:08:09 +0300

    Added carousel slider in RegisterPage

--- client/src/pages/auth/RegisterPage.jsx
+++ client/src/pages/auth/RegisterPage.jsx
@@ -1,7 +1,7 @@
-import { Button, Form, Input } from "antd";
+import { Button, Form, Input, Carousel } from "antd";
 import React from "react";
 import { Link } from "react-router-dom";
-
+import AuthCarousel from "../../components/auth/AuthCarousel";
 export const RegisterPage = () => {
   return (
     <div className="h-screen">
@@ -9,30 +9,30 @@ export const RegisterPage = () => {
         <div className="xl:px-20 px-10 w-full flex flex-col h-full justify-center relative">
           <img
             src="https://www.motifsoft.com/wp-content/uploads/2023/11/MotifSoft-logo-colored-1.png"
-            width="200px"
+            width="300px"
             alt="MOTİF SOFT"
-            className="mb-5"
-          ></img>
+            className="pb-10"
+          />
           <Form layout="vertical">
             <Form.Item
               label="Kullanıcı Adı"
-              name="username"
+              name={"username"}
               rules={[
                 {
                   required: true,
-                  message: "Kullanıcı adı alanı boş bırakılamaz!",
+                  message: "Kullanıcı Adı Alanı Boş Bırakılamaz!",
                 },
               ]}
             >
-              <Input></Input>
+              <Input />
             </Form.Item>
             <Form.Item
-              label="E-posta Adresiniz"
-              name="email"
+              label="E-mail"
+              name={"email"}
               rules={[
                 {
                   required: true,
-                  message: "E-posta alanı boş bırakılamaz!",
+                  message: "E-mail Alanı Boş Bırakılamaz!",
                 },
               ]}
             >
@@ -40,11 +40,11 @@ export const RegisterPage = () => {
             </Form.Item>
             <Form.Item
               label="Şifre"
-              name="password"
+              name={"password"}
               rules={[
                 {
                   required: true,
-                  message: "Şifre alanı boş bırakılamaz!",
+                  message: "Şifre Alanı Boş Bırakılamaz!",
                 },
               ]}
             >
@@ -52,11 +52,11 @@ export const RegisterPage = () => {
             </Form.Item>
             <Form.Item
               label="Şifre Tekrar"
-              name="passwordAgain"
+              name={"passwordAgain"}
               rules={[
                 {
                   required: true,
-                  message: "Şifre tekrar alanı boş bırakılamaz!",
+                  message: "Şifre Tekrar Alanı Boş Bırakılamaz!",
                 },
               ]}
             >
@@ -66,18 +66,48 @@ export const RegisterPage = () => {
               <Button
                 type="primary"
                 htmlType="submit"
-                size="large"
                 className="w-full"
+                size="large"
               >
                 Kaydol
               </Button>
             </Form.Item>
           </Form>
-          <div className="flex justify-center absolute left-0 bottom-20 w-full">
-            Bir hesabınız mı var? <Link to="/login" className="text-blue-700">&nbsp;Giriş Yap</Link>
+          <div className="flex justify-center absolute left-0 bottom-10 w-full">
+            Bir hesabınız var mı?&nbsp;
+            <Link to="/login" className="text-blue-600">
+              Şimdi giriş yap
+            </Link>
           </div>
         </div>
-        <div className="xl:w-4/6 min-w-[600px]"></div>
+        <div className="xl:w-4/6 lg:w-3/5 md:w-1/2 md:flex hidden bg-[#6c63ff] h-full">
+          <div className="w-full h-full flex items-center">
+            <div className="w-full">
+              <Carousel className="!h-full px-6">
+                <AuthCarousel
+                  img="/images/responsive.svg"
+                  title="Responsive"
+                  desc="Tüm Cihaz Boyutlarıyla Uyumluluk"
+                />
+                <AuthCarousel
+                  img="/images/statistic.svg"
+                  title="İstatistikler"
+                  desc="Geniş Tutulan İstatistikler"
+                />
+                <AuthCarousel
+                  img="/images/customer.svg"
+                  title="Müşteri Memnuniyeti"
+                  desc="Deneyim Sonunda Üründen Memnun Müşteriler"
+                />
+                <AuthCarousel
+                  img="/images/admin.svg"
+                  title="Yönetici Paneli"
+                  desc="Tek Yerden Yönetim"
+                />
+              </Carousel>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
