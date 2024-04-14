ebf288d4ddfff81a28037933e1c9235d8a1723d7
Fatma Betül Sofuoğlu <40726607+fatmabetulsofuoglu@users.noreply.github.com>
2024-04-15 02:14:19 +0300

    Category CRUD operations

--- api/server.js
+++ api/server.js
@@ -2,9 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = express();
+const cors = require("cors");
 
 const port = 6000;
-
+const categoryRoute = require("./routes/categories.js");
 dotenv.config();
 
 const connect = async () => {
@@ -16,7 +17,11 @@ const connect = async () => {
   }
 };
 
-app.get("/", (req, res) => res.send("Hello World!"));
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+app.use("/api", categoryRoute);
 
 app.listen(port, () => {
   connect();
