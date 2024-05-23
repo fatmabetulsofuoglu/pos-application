8785c9ff38d85ea7bb1d0e0fc93498f35e3d875f
Fatma Betül Sofuoğlu <40726607+fatmabetulsofuoglu@users.noreply.github.com>
2024-05-23 12:14:23 +0300

    Logout

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,5 +1,5 @@
-import { Link } from "react-router-dom";
-import { Badge, Input } from "antd";
+import { Link, useNavigate } from "react-router-dom";
+import { Badge, Input, message } from "antd";
 import {
   SearchOutlined,
   HomeOutlined,
@@ -14,6 +14,16 @@ import "./style.css";
 
 const Header = () => {
   const cart = useSelector((state) => state.cart);
+  const navigate = useNavigate();
+
+  const logOut = () => {
+    if (window.confirm("Çıkış yapmak istediğinize emin misiniz?")) {
+      localStorage.removeItem("posUser");
+      navigate("/login");
+      message.success("Çıkış yaptınız");
+    }
+  };
+
   return (
     <div className="border-b mb-6">
       <header className="py-4 px-6 flex justify-between items-center gap-10">
@@ -64,20 +74,19 @@ const Header = () => {
             <BarChartOutlined className="md:text-2xl text-xl" />
             <span className="md:text-xs text-[10px]">İstatistikler</span>
           </Link>
-          <Link to={"/"} className="menu-link">
-            <LogoutOutlined className="md:text-2xl text-xl" />
-            <span className="md:text-xs text-[10px]">Çıkış</span>
-          </Link>
+          <div onClick={logOut}>
+            <Link className="menu-link">
+              <LogoutOutlined className="md:text-2xl text-xl" />
+              <span className="md:text-xs text-[10px]">Çıkış</span>
+            </Link>
+          </div>
         </div>
         <Badge
           count={cart.cartItems.length}
           offset={[0, 6]}
           className="md:hidden flex"
         >
-          <Link
-            to={"/cart"}
-            className="menu-link"
-          >
+          <Link to={"/cart"} className="menu-link">
             <ShoppingCartOutlined className="text-2xl" />
             <span className="md:text-xs text-[10px]">Sepet</span>
           </Link>
